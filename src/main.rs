bb1838ce1ea3257b176aab0e3b67b897831cb36a
Pikkel <76259034+Pikkel@users.noreply.github.com>
2021-10-20 22:11:25 -0400

    Add files via upload

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+use std::time::SystemTime;
+// RUST NUMBER ONE 💪🇵🇸💪🇵🇸💪🇵🇸
+
+fn main() {
+    let st = SystemTime::now();
+    println!("start");
+    for num in 1..100001 {
+        let mut num = num;
+        loop {
+            if num == 1 {
+                break;
+            }
+            if (num % 2) == 0 {
+                num = num / 2;
+                continue;
+            }
+            num = (3 * num) + 1;
+        }
+    }
+    println!("fin");
+    let nst = SystemTime::now();
+    let diff = nst.duration_since(st);
+    println!("{:?}", diff);
+}
